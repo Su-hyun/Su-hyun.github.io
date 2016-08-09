c7d32e52072adba607f5359f253c53b55840e424
Su-hyun <endband1978@gmail.com>
2016-08-09 15:43:33 +0900

    08.09 - 페이드 -> 슬라이드 변경

--- partials/layout/contents/controller.js
+++ partials/layout/contents/controller.js
@@ -2,9 +2,9 @@
 
 define(['projectSugar'], function () {
   console.log("메인");
-  var contentsModule = angular.module('projectSugar', ['ui.router','oc.lazyLoad']);
+  var contentsModule = angular.module('projectSugar', ['ui.router','oc.lazyLoad','layoutService']);
 
-  contentsModule.controller('contentsController', ['$scope','$ocLazyLoad',
+  contentsModule.controller('contentsController', ['$scope','$ocLazyLoad','layoutService',
     function($scope, $ocLazyLoad,layoutService) {
 
       // 슬라이드
@@ -101,6 +101,8 @@ define(['projectSugar'], function () {
             './partials/common/js/jquery.cookie.js'
           ])
           .then(function() {
+            //contentsService.fire();
+            //layoutService.fire();
 
             var $eventArea = $('.event-area'),
                 $eventList = $('.event-list'),
@@ -116,31 +118,48 @@ define(['projectSugar'], function () {
 
             $eventList.eq(idx).addClass('on');
             var slideRel = setInterval(function () {
+              slideFn ($eventList, idx, 0, "-100%", 1000);
               idx ++;
-              fadeFn ($eventList, idx, 2000);
-            },4000);
+              //  fadeFn ($eventList, idx, 2000);
+              slideFn ($eventList, idx, "100%", 0, 1000);
 
-            $eventArea.on({
-              mouseenter : function () {
-                clearInterval(slideRel)
-              },
-              mouseleave : function () {
-                slideRel = setInterval(function () {
-                  idx++;
-                  fadeFn ($eventList, idx, 2000);
-                },4000);
-              }
-            });
-            function fadeFn (select, index, sec) {
+            },3000);
+
+            //$eventArea.on({
+            //  mouseenter : function () {
+            //    clearInterval(slideRel)
+            //  },
+            //  mouseleave : function () {
+            //    slideRel = setInterval(function () {
+            //      idx++;
+            //      fadeFn ($eventList, idx, 2000);
+            //    },4000);
+            //  }
+            //});
+            //function fadeFn (select, index, sec) {
+            //  var leng = select.length;
+            //  select.eq(index).find('.event-area').fadeIn(sec)
+            //      .parent('li').addClass('on')
+            //      .siblings().removeClass('on').find('.event-area')
+            //      .fadeOut(sec);
+            //  if(index == leng){
+            //    idx = 0;
+            //    fadeFn (select, idx, sec);
+            //  }
+            //}
+
+            function slideFn (select, index, start, end, sec) {
               var leng = select.length;
-              select.eq(index).find('.event-area').fadeIn(sec)
-                  .parent('li').addClass('on')
-                  .siblings().removeClass('on').find('.event-area')
-                  .fadeOut(sec);
+              select.eq(index).addClass('on').find('.event-area').css({display:'block',left:start})
+                  .stop().animate({left:end},sec).parent('li').siblings('li').removeClass('on');
               if(index == leng){
                 idx = 0;
-                fadeFn (select, idx, 2000);
+                slideFn (select, idx, start, end, sec)
+              }else if(index < 0){
+                idx = leng - 1;
+                slideFn (select, idx, start, end, sec)
               }
+
             }
           });
 
