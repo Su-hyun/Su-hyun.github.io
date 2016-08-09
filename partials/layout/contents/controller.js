360d5d15de745f6f65aa787701d1f1bae7ffd708
Su-hyun <endband1978@gmail.com>
2016-08-09 14:46:27 +0900

    08.09 - layoutService 삭제

--- partials/layout/contents/controller.js
+++ partials/layout/contents/controller.js
@@ -2,9 +2,9 @@
 
 define(['projectSugar'], function () {
   console.log("메인");
-  var contentsModule = angular.module('projectSugar', ['ui.router','oc.lazyLoad','layoutService']);
+  var contentsModule = angular.module('projectSugar', ['ui.router','oc.lazyLoad']);
 
-  contentsModule.controller('contentsController', ['$scope','$ocLazyLoad','layoutService',
+  contentsModule.controller('contentsController', ['$scope','$ocLazyLoad',
     function($scope, $ocLazyLoad,layoutService) {
 
       // 슬라이드
@@ -101,8 +101,47 @@ define(['projectSugar'], function () {
             './partials/common/js/jquery.cookie.js'
           ])
           .then(function() {
-            //contentsService.fire();
-            layoutService.fire();
+
+            var $eventArea = $('.event-area'),
+                $eventList = $('.event-list'),
+                idx = 0;
+
+            $(window).resize(function () {
+              var eventWID = $('.event-con').width(),
+                  movetxt01HEI = $('.move-txt01').height();
+              $('.move-txt02').css('height',movetxt01HEI);
+              $eventList.eq(0).css('margin-left' , -eventWID * 0.23);
+            });
+            $(window).trigger("resize");
+
+            $eventList.eq(idx).addClass('on');
+            var slideRel = setInterval(function () {
+              idx ++;
+              fadeFn ($eventList, idx, 2000);
+            },4000);
+
+            $eventArea.on({
+              mouseenter : function () {
+                clearInterval(slideRel)
+              },
+              mouseleave : function () {
+                slideRel = setInterval(function () {
+                  idx++;
+                  fadeFn ($eventList, idx, 2000);
+                },4000);
+              }
+            });
+            function fadeFn (select, index, sec) {
+              var leng = select.length;
+              select.eq(index).find('.event-area').fadeIn(sec)
+                  .parent('li').addClass('on')
+                  .siblings().removeClass('on').find('.event-area')
+                  .fadeOut(sec);
+              if(index == leng){
+                idx = 0;
+                fadeFn (select, idx, 2000);
+              }
+            }
           });
 
 
