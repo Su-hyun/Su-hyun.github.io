aeee1667463a05403d8fac9007f5bf80d569f59f
Su-hyun <endband1978@gmail.com>
2016-08-09 15:45:49 +0900

    08.09 - gnb 호버 변경

--- partials/layout/layoutService.js
+++ partials/layout/layoutService.js
@@ -1,63 +1,29 @@
 'use strict';
 
 define([ 'projectSugar' ], function() {
-	var layoutServiceModule = angular.module('layoutServiceModule', []);
+  var layoutServiceModule = angular.module('layoutServiceModule', []);
 
-	layoutServiceModule.factory('layoutService', [ '$rootScope',
-			function($rootScope) {
-				return {
-					fire : function() {
-					  var $eventArea = $('.event-area'),
-                $eventList = $('.event-list'),
-                idx = 0;
+  layoutServiceModule.factory('layoutService', [ '$rootScope',
+    function($rootScope) {
+      return {
+        fire : function() {
+          var idx = 0;
 
-            $(window).resize(function () {
-              var eventWID = $('.event-con').width(),
-                  movetxt01HEI = $('.move-txt01').height();
-              $('.move-txt02').css('height',movetxt01HEI);
-              $eventList.eq(0).css('margin-left' , -eventWID * 0.23);
-            });
-            $(window).trigger("resize");
+          $('.lnb > li').on('click', function(){
+            idx = $ (this).find ('.sub-lnb > li').length;
+            var $this = $(this),
+                liHEI = $ (this).find ('.sub-lnb > li').innerHeight ();
 
-            $('.lnb > li').on({
-              mouseenter: function () {
-                idx = $ (this).find ('.sub-lnb > li').length;
-                var liHEI = $ (this).find ('.sub-lnb > li').innerHeight ();
-                $ (this).children ('.sub-lnb').css ("height", liHEI * idx)
-              },
-              mouseleave: function () {
-                $ (this).children ('.sub-lnb').css ('height', 0)
-              }
-            });
-
-            $eventList.eq(idx).addClass('on');
-            var slideRel = setInterval(function () {
-              idx ++;
-              fadeFn ($eventList, idx, 2000);
-            },4000);
-            $eventArea.on({
-              mouseenter : function () {
-                clearInterval(slideRel)
-              },
-              mouseleave : function () {
-                slideRel = setInterval(function () {
-                  idx++;
-                  fadeFn ($eventList, idx, 2000);
-                },4000);
-              }
-            });
-            function fadeFn (select, index, sec) {
-              var leng = select.length;
-              select.eq(index).find('.event-area').fadeIn(sec)
-                  .parent('li').addClass('on')
-                  .siblings().removeClass('on').find('.event-area')
-                  .fadeOut(sec);
-              if(index == leng){
-                idx = 0;
-                fadeFn (select, idx, 2000);
-              }
+            if(!$this.is('.active')) {
+              $this.addClass('active');
+              $this.children ('.sub-lnb').css ("height", liHEI * idx);
+              $this.siblings().removeClass('active').children ('.sub-lnb').css ('height', 0);
+            }else if($this.is('.active')) {
+              $this.removeClass('active');
+              $this.children ('.sub-lnb').css ('height', 0)
             }
-					}// fire end
-				};// return end
-			}]);// .define function end
+          });
+        }// fire end
+      };// return end
+    }]);// .define function end
 });
