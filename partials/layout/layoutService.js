1e0553778b7af023d03d0b4ad14add1b51ea4a06
Su-hyun <endband1978@gmail.com>
2016-08-12 14:01:26 +0900

    08.12 code refactor

--- partials/layout/layoutService.js
+++ partials/layout/layoutService.js
@@ -6,14 +6,6 @@ define([ 'projectSugar' ], function() {
 			function($rootScope) {
 				return {
 					fire : function() {
-            var $eventList = $('.event-list');
-            $(window).resize(function () {
-              var eventWID = $('.event-con').width(),
-                  movetxt01HEI = $('.move-txt01').height();
-              $('.move-txt02').css('height',movetxt01HEI);
-              $eventList.eq(0).css('margin-left' , -eventWID * 0.23);
-            });
-            $(window).trigger("resize");
             $(".requireLoadingText").remove();
 					}// fire end
 				};// return end
