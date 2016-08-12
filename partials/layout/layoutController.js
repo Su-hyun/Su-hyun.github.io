4b0bc6f8b728dea9b6d14e4a6e6f0bb5ed71742d
Su-hyun <endband1978@gmail.com>
2016-08-12 13:52:47 +0900

    08.12 loadding & 구조조정

--- partials/layout/layoutController.js
+++ partials/layout/layoutController.js
@@ -432,14 +432,6 @@ define(
             var $eventArea = $('.event-area'),
                 $eventList = $('.event-list'),
                 idx = 0;
-            $(window).resize(function () {
-              var eventWID = $('.event-con').width(),
-                  movetxt01HEI = $('.move-txt01').height();
-              $('.move-txt02').css('height',movetxt01HEI);
-              $eventList.eq(0).css('margin-left' , -eventWID * 0.23);
-            });
-            $(window).trigger("resize");
-            //
             $eventList.eq(idx).addClass('on');
             var slideRel = setInterval(function () {
               //  slideFn ($eventList, idx, 0, "-100%", 1000);
